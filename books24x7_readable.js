9dd8a2c6933da313910e96777e4602be4fb76104
Zhang Tai <me@zhangtai.me>
2014-04-28 14:01:17 +0800

    Add mini map

--- books24x7_readable.js
+++ books24x7_readable.js
@@ -3,7 +3,7 @@
 // @namespace	http://zhangtai.me
 // @description	Change fonts on books24x7
 // @author	Zhang Tai
-// @version	1.2
+// @version	1.3
 // @require jquery.js
 // @include	http://www.books24x7.com/assetviewer.aspx*
 // ==/UserScript==
@@ -29,27 +29,19 @@ $(document).ready(function(){
 	head.appendChild(style);
     
 })({ family:'Antic Slab', style:['400','700'] });
-
-
-  
-$( "#ctl00_ContentPlaceHolder1_TopProgressControl_PreviousSection" ).attr( "src", "//icons.iconarchive.com/icons/visualpharm/ios7v2/32/Arrows-Back-icon.png" );
-$( "#ctl00_ContentPlaceHolder1_TopProgressControl_NextSection" ).attr( "src", "//icons.iconarchive.com/icons/visualpharm/ios7v2/32/Arrows-Forward-icon.png" );
+    
+$("#download").after("<a id='newPrevPage' href='#'><img src='//icons.iconarchive.com/icons/visualpharm/ios7v2/32/Arrows-Back-icon.png' /></a><a id='newNextPage' href='#'><img src='//icons.iconarchive.com/icons/visualpharm/ios7v2/32/Arrows-Forward-icon.png' /></a>");
+    
+    $("#newPrevPage").click(function() {
+        $( "#ctl00_ContentPlaceHolder1_TopProgressControl_PreviousSection" ).click();
+    });
+    $("#newNextPage").click(function() {
+        $( "#ctl00_ContentPlaceHolder1_TopProgressControl_NextSection" ).click();
+    });
     
 $( "#FullText1" ).before( "<a href='http://www.books24x7.com/bookshelf.asp'><img src='//png-1.findicons.com/files/icons/1580/devine_icons_part_2/24/home.png' /></a>" );
 $( "#UnattachedAnnotationsHere" ).after("<div class='progress'><div id='progress-bar' class='progress-bar' role='progressbar' aria-valuenow='60' aria-valuemin='0' aria-valuemax='100' style='width: " + progressDig + "%;'>" + progressRaw + "</div></div>");
 $( "#FullText1" ).before('<a href="#" id="goodreadProgress" title"Find in Goodread" alt"Goodread"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAARVBMVEUAAACkiHGkiHGkiHGkiHGkiHGkiHGkiHGpj3jt7N7k3tDy8uXGt6TQxLPVy7q4o46znIeulYDLvavBsJ3f2Mjp5de8qZVgnfm3AAAAB3RSTlMAYNAQ8LDAThnSHAAAAHhJREFUeF5lz0mugzAAA9AkQF4GZvr%2F%2FY9aobKoVC%2Ffxna4EyeYYvhkSABpuGEYsZ1z2RhvSVjqsrSOFELE0XfmDjFkzA2lQw5wvqCDAFfF9gWlro72BfZX%2F7seyCgb9gr5qZ2VtkL8DPtvva4F6ZkOMA6%2F537uvwGpugbVLOx6dAAAAABJRU5ErkJggg%3D%3D" /></a>');
-    
-$( "#leftHandNavToggle" ).click(function() {
-  $( "#MenuTD" ).toggle( "slow", function() {
-    // Animation complete.
-  });
-  $( "#ctl00_ContentPlaceHolder1_AssetMetaControl_bookseparator" ).toggle( "slow", function() {
-    // Animation complete.
-  });
-  $( "#ctl00_ContentPlaceHolder1_AssetMetaUpdatePanel" ).toggle( "slow", function() {
-    // Animation complete.
-  });
-});
     
 $( ".progress" ).click(function() {
   $( "#MenuTD" ).toggle( "slow", function() {
@@ -66,5 +58,17 @@ $( ".progress" ).click(function() {
     $( "#goodreadProgress" ).click(function(){
         window.open('https://www.goodreads.com/search?utf8=✓&query=' + bookTitle );
     });
-                                   
+            
+    
+    // [MiniMap]
+
+	var miniMapScript=document.createElement("script"); 
+    miniMapScript.src="http://dropthebit.com/demos/mini_page_nav/miniPageNav.js"; 
+    document.body.appendChild(miniMapScript);
+
+    
+    $('.progress').hover(function() {
+        $('.canvasPaneHandler').parent().toggle();
+    });
+   
 });
