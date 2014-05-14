f84d711efd9dbdf55e82d64c979ff8df197d87da
Zhang Tai <tai.zhang@outlook.com>
2014-05-14 16:47:12 +0800

    Update books24x7_readable.js

--- books24x7_readable.js
+++ books24x7_readable.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name	books24x7_readable
+// @name	books24x7_readable - (chapter section)
 // @namespace	http://zhangtai.me
-// @description	Change fonts on books24x7
+// @description	New version - (chapter section)
 // @author	Zhang Tai
 // @version	1.3
 // @require jquery.js
@@ -9,17 +9,12 @@
 // ==/UserScript==
 
 $(document).ready(function(){
-    
-    progressRaw = $(".b24-chunknavigate img[title]").attr("title");
-    progressDig = $(".b24-chunknavigate img[title]").attr("title").match(/\d+/g);
-    bookTitle = $("div#FullText2 span").contents().text();
-
-    
+    // Font change
     (function(font) {
 	var	head = document.getElementsByTagName('head')[0],
 		link = document.createElement('link'),
 		style = document.createElement('style'),
-		rules = document.createTextNode('#ctl00_ContentPlaceHolder1_ContentPanel * { font-family: "' + font.family + '", arial, sans-serif !important }');
+		rules = document.createTextNode('#ctl00_ContentPlaceHolder1_ContentPanel * { font-family: "' + font.family + '", arial, sans-serif }');
 
     link.rel  = 'stylesheet';
 	link.href = '//fonts.googleapis.com/css?family=' + font.family + ':' + (font.style || []) + '&subset=' + (font.subset || ['latin']);
@@ -28,47 +23,87 @@ $(document).ready(function(){
 	style.styleSheet ? style.styleSheet.cssText = rules.nodeValue : style.appendChild(rules);
 	head.appendChild(style);
     
-})({ family:'Antic Slab', style:['400','700'] });
+    })({ family:'Alef', style:['400','700'] });   //End of the fonts, candidates: Open Sans Condensed, Antic Slab, Bitter, Nunito, Alef
     
-$("#download").after("<a id='newPrevPage' href='#'><img src='//icons.iconarchive.com/icons/visualpharm/ios7v2/32/Arrows-Back-icon.png' /></a><a id='newNextPage' href='#'><img src='//icons.iconarchive.com/icons/visualpharm/ios7v2/32/Arrows-Forward-icon.png' /></a>");
+    // Page navi panel
+	$("#download").after("<a id='newPrevPage' href='#'><span class='pageNaviIndictor'><</span></a><a id='newNextPage' href='#'><span class='pageNaviIndictor'>></span></a>");  
     
-    $("#newPrevPage").click(function() {
-        $( "#ctl00_ContentPlaceHolder1_TopProgressControl_PreviousSection" ).click();
+    // Get all current chapter articles
+    var bookID = $('#ctl00_ContentPlaceHolder1_ChapterMenuSection_AssetIdHidden').attr('value');
+    
+    // table content change
+    $('#divChapterTOCMenu').append('<div id="newChapterSessions" class="ui vertical menu"></div>');
+    if (! $('div.b24-chaptertoc2-selected > a').text() == '') {
+        $('#newChapterSessions').append('<a href="#" class="item">' + $('div.b24-chaptertoc2-selected > a').text() + '</a>');
+    };    
+//    $('#newChapterSessions').append('<a href="#" class="item">' + $('div.b24-chaptertoc2-selected > a').text() + '</a>');
+    
+    var chapterSectionTitle = new Array();
+    $('a.b24-chaptertoc2').each(function(){
+        $('#newChapterSessions').append('<a href="#" class="item">' + $(this).text() + '</a>');
+        chapterSectionTitle.push($(this).text());
     });
-    $("#newNextPage").click(function() {
-        $( "#ctl00_ContentPlaceHolder1_TopProgressControl_NextSection" ).click();
-    });
+    $('#newChapterSessions a.item:first-child').addClass('active');
+    chapterSectionTitle.unshift($('div.b24-chaptertoc2-selected > a').text());
     
-$( "#FullText1" ).before( "<a href='http://www.books24x7.com/bookshelf.asp'><img src='//png-1.findicons.com/files/icons/1580/devine_icons_part_2/24/home.png' /></a>" );
-$( "#UnattachedAnnotationsHere" ).after("<div class='progress'><div id='progress-bar' class='progress-bar' role='progressbar' aria-valuenow='60' aria-valuemin='0' aria-valuemax='100' style='width: " + progressDig + "%;'>" + progressRaw + "</div></div>");
-$( "#FullText1" ).before('<a href="#" id="goodreadProgress" title"Find in Goodread" alt"Goodread"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAARVBMVEUAAACkiHGkiHGkiHGkiHGkiHGkiHGkiHGpj3jt7N7k3tDy8uXGt6TQxLPVy7q4o46znIeulYDLvavBsJ3f2Mjp5de8qZVgnfm3AAAAB3RSTlMAYNAQ8LDAThnSHAAAAHhJREFUeF5lz0mugzAAA9AkQF4GZvr%2F%2FY9aobKoVC%2Ffxna4EyeYYvhkSABpuGEYsZ1z2RhvSVjqsrSOFELE0XfmDjFkzA2lQw5wvqCDAFfF9gWlro72BfZX%2F7seyCgb9gr5qZ2VtkL8DPtvva4F6ZkOMA6%2F537uvwGpugbVLOx6dAAAAABJRU5ErkJggg%3D%3D" /></a>');
-    
-$( ".progress" ).click(function() {
-  $( "#MenuTD" ).toggle( "slow", function() {
-    // Animation complete.
-  });
-  $( "#ctl00_ContentPlaceHolder1_AssetMetaControl_bookseparator" ).toggle( "slow", function() {
-    // Animation complete.
-  });
-  $( "#ctl00_ContentPlaceHolder1_AssetMetaUpdatePanel" ).toggle( "slow", function() {
-    // Animation complete.
-  });
-});
+    // Change first section ID
+    var chunkid = new Array();
+      $('#divChapterTOCMenu table input').each(function(){
+          chunkid.push($(this).attr('value'));
+        });
+    $('#ctl00_ContentPlaceHolder1_ContentPanel > div > div.chapter').attr("id", chunkid[0]);
+      // Load the contents
+      for (var i = 1; i < chunkid.length; i++) {
+        $('#ctl00_ContentPlaceHolder1_ContentPanel > div').append("<div id='" + chunkid[i] + "' class='chapter' style='display:none;'></div>");
+        $('#' + chunkid[i]).load("assetviewer.aspx?bookid=" + bookID + "&chunkid=" + chunkid[i] + " #ctl00_ContentPlaceHolder1_ContentPanel div.chapter > .section");
 
-    $( "#goodreadProgress" ).click(function(){
-        window.open('https://www.goodreads.com/search?utf8=✓&query=' + bookTitle );
-    });
-            
+      }
     
-    // [MiniMap]
-
-	var miniMapScript=document.createElement("script"); 
-    miniMapScript.src="http://dropthebit.com/demos/mini_page_nav/miniPageNav.js"; 
-    document.body.appendChild(miniMapScript);
-
+	
     
-    $('.progress').hover(function() {
-        $('.canvasPaneHandler').parent().toggle();
+    // Page navigation.
+    var chunkIndex = 0;
+    var initProgressChapter = Math.round((chunkIndex + 1) * 100 / chunkid.length);
+    $( "#UnattachedAnnotationsHere" ).after("<div class='progress'><div id='progress-bar' class='progress-bar' role='progressbar' aria-valuenow='60' aria-valuemin='0' aria-valuemax='100' style='width: " + initProgressChapter + "%;'></div></div>");
+	$( ".progress" ).click(function() {
+      $( "#MenuTD" ).toggle( "fast" );
     });
-   
+    
+    $('#newNextPage').click(function() {
+      if (chunkIndex < chunkid.length - 1) {
+          $('#' + chunkid[chunkIndex]).removeClass('animated fadeInRight fadeInLeft');
+          $('#newChapterSessions a:nth-child(' + (chunkIndex + 1) + ')').removeClass('active');
+          $('#' + chunkid[chunkIndex]).hide();
+          $('#' + chunkid[chunkIndex + 1]).show();
+          $('#' + chunkid[chunkIndex + 1]).addClass('animated fadeInRight');
+          chunkIndex += 1;
+          console.log(chunkIndex);
+          $('#newChapterSessions a:nth-child(' + (chunkIndex + 1) + ')').addClass('active');
+          var progressChapter = Math.round((chunkIndex + 1) * 100 / chunkid.length);
+          $('#progress-bar').css("width", progressChapter + "%");
+      }
+    });
+    
+    $('#newPrevPage').click(function() {
+      if (chunkIndex > 0) {
+          $('#' + chunkid[chunkIndex]).removeClass('animated fadeInRight fadeInLeft');
+          $('#newChapterSessions a:nth-child(' + (chunkIndex + 1) + ')').removeClass('active');
+          $('#' + chunkid[chunkIndex]).hide();
+          $('#' + chunkid[chunkIndex - 1]).show();
+          $('#' + chunkid[chunkIndex - 1]).addClass('animated fadeInLeft');
+          chunkIndex -= 1;
+          $('#newChapterSessions a:nth-child(' + (chunkIndex + 1) + ')').addClass('active');
+          console.log(chunkIndex);
+          var progressChapter = Math.round((chunkIndex + 1) * 100 / chunkid.length);
+          $('#progress-bar').css("width", progressChapter + "%");
+      }
+    });        
+        
+    $('#MenuTD').prepend($('#ctl00_ContentPlaceHolder1_AssetMetaUpdatePanel'));
+    $('#container #ctl00_ContentPlaceHolder1_AssetMetaUpdatePanel').remove();
+    
+    $('#ctl00_ContentPlaceHolder1_AssetMetaUpdatePanel a > div > img').hover(function() {
+      $( "#ctl00_ContentPlaceHolder1_AssetMetaControl_bookseparator" ).toggle( "fast" );
+    });
+    
 });
